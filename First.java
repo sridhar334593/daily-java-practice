4bf85a5ecf51e0325100f556096a7200b0357cc5
sridhar <sridharahdirs33@gmail.com>
2026-02-23 09:39:07 +0530

    Update First.java

--- First.java
+++ First.java
@@ -3,5 +3,7 @@ public class First {
      public static void main(String[] args) {
       System.out.println("hello world");
       System.out.println("hello world");
+        System.out.println("hello world");
+     
      }
 }
