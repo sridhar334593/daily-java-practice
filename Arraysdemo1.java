184691bc10407917fbc6bf431d5866c4be16b3d9
sridhar <sridharahdirs33@gmail.com>
2026-02-25 23:02:39 +0530

    add file

---
+++ Arraysdemo1.java
@@ -0,0 +1,10 @@
+public class Arraysdemo1 {
+  public static void main(String[] args) {
+    int a[][] = new int[3][4];
+
+    a[0][0] = 85;
+    a[2][3] = 78;
+    System.out.println(a[2][3]);
+  }
+  
+}
