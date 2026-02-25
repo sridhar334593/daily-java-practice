d0a75e233e35f79fdc77d927ae8d1ea5fa884687
sridhar <sridharahdirs33@gmail.com>
2026-02-25 22:57:32 +0530

    add file

---
+++ Arraysdemo.java
@@ -0,0 +1,21 @@
+public class Arraysdemo {
+  public static void main(String[] args) {
+    int marks[];
+    marks=new int[6];
+    //   int marks[]=new marks[6];
+    marks[0]=95;
+    marks[1]=56;
+    marks[2]=76;
+    marks[3]=98;
+    marks[4]=67;
+    marks[5]=89;
+
+    /* 
+    int marks[]={95,56,76,98,67,89};
+    */
+    
+
+    System.out.println(marks[4]);
+
+  }
+}
