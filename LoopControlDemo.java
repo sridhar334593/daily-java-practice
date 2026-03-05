626ae0040a58d3939e11370460e91d9a7ed03dc3
sridhar <sridharahdirs33@gmail.com>
2026-03-05 12:09:08 +0530

    add file

--- LoopControlDemo.java
+++ LoopControlDemo.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+
 public class LoopControlDemo {
-  
+  public static void main(String[] args) {
+    double n ,sum=0.0;
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter the number .Enter a negative number to stop:\n");
+    
+    while(true){
+      n = scanner.nextDouble();
+      if(n==0.0){
+        break;
+      }
+      sum = sum+n;
+
+    }
+    System.out.println("sum is"  + sum);
+
+
+  }
 }
