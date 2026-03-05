9e1cc0192985ef56da7d7033050f84af10123fe6
sridhar <sridharahdirs33@gmail.com>
2026-03-05 12:09:30 +0530

    add file

--- NestedloopDemo.java
+++ NestedloopDemo.java
@@ -1,7 +1,9 @@
 public class NestedloopDemo {
   public static void main(String[] args) {
-
+   
+    outerloop:  //labels
     for(int i=1; i<=5; i++){
+      innerloop: //labels
       for(int j=1; j<=5-i; j++){
         System.out.print(" ");
     }
