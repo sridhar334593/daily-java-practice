0a58b043aba3ddc49ef00bebf08c0bcc7d338ec9
sridhar <sridharahdirs33@gmail.com>
2026-03-05 07:20:39 +0530

    add file

---
+++ loopdemo.java
@@ -0,0 +1,21 @@
+public class loopdemo {
+  public static void main(String[] args) {
+    int i;
+    i=1;
+   /*  for(i=1; i<=5; i++){
+      System.out.println(counter);
+    } */
+
+   /* while(i<=5) {
+    System.out.println(i);
+    i++;
+   } */
+  
+   do{
+    System.out.println(i);
+    i++;
+   } while(i<=6);
+  }
+
+  
+}
